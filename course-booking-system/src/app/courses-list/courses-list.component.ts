234a2ec5e38cd7322bd4fc11e9081d35c096fa28
Noemi <noemitortora222@gmail.com>
2025-05-23 15:29:19 +0200

    creato un nuovo componente course-card implementato un if, pipe e iniziato l' input

--- course-booking-system/src/app/courses-list/courses-list.component.ts
+++ course-booking-system/src/app/courses-list/courses-list.component.ts
@@ -10,11 +10,12 @@ import { CommonModule } from '@angular/common';
 })
 export class CoursesListComponent {
   titolo: string = 'Corsi disponibili';
-
   courses = [
-    {id: 1, title: 'Corso di Angular', description: 'Impara le basi di Angular', duration: 30, price: 200, soldOut: false, img:'foto1.png' },
-    {id: 2, title: 'Corso di React', description: 'Impara le basi di React', duration: 30, price: 200, soldOut: true, img:'foto2.jpeg' },
+    {id: 1, title: 'Corso di Angular', description: 'Impara le basi di Angular', date: '2023-03-23', price: 200, soldOut: false, img:'foto1.png' },
+    {id: 2, title: 'Corso di React', description: 'Impara le basi di React', date: '2022-02-23', price: 200, soldOut: true, img:'foto2.jpeg' }
   ];
+
+
 
 viewDetails(corso: any): void {
   alert(`Dettagli del corso per ${corso.title}`);
