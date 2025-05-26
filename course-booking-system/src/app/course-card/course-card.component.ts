25b776105fbf09a9f048517d8f785e3ad6063271
Noemi <noemitortora222@gmail.com>
2025-05-26 14:58:46 +0200

    decoratore output, emettitori di eventi, aggiunta di una wishilist e creazione di un modello

--- course-booking-system/src/app/course-card/course-card.component.ts
+++ course-booking-system/src/app/course-card/course-card.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-course-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
 export class CourseCardComponent {
+  @Input() corso: any;
+  @Output() courseBooked = new EventEmitter<any>();
+  @Output() wishlistAdded = new EventEmitter<any>();
 
+
+  onCourseBooked(): void {
+    this.courseBooked.emit(this.corso);
+  }   
+
+  onAddToWishlist(): void {
+    this.wishlistAdded.emit(this.corso);
+  }   
 }
--- course-booking-system/src/app/courses-list/courses-list.component.ts
+++ course-booking-system/src/app/courses-list/courses-list.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CourseCardComponent } from '../course-card/course-card.component';
 
 @Component({
   selector: 'app-courses-list',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule,CourseCardComponent], 
   templateUrl: './courses-list.component.html',
   styleUrl: './courses-list.component.css'
 })
 export class CoursesListComponent {
   titolo: string = 'Corsi disponibili';
+  wishlist: any[] = [];
   courses = [
     {id: 1, title: 'Corso di Angular', description: 'Impara le basi di Angular', date: '2023-03-23', price: 200, soldOut: false, img:'foto1.png' },
     {id: 2, title: 'Corso di React', description: 'Impara le basi di React', date: '2022-02-23', price: 200, soldOut: true, img:'foto2.jpeg' }
   ];
 
-
-
-viewDetails(corso: any): void {
-  alert(`Dettagli del corso per ${corso.title}`);
+  onCourseBooked(course: any): void {
+    console.log(`il genitore ha saputo della prenotazione:` ,course.title);
 }
 
-
+onWishlistAdded(course: any): void {
+    console.log(`il genitore ha saputo dell'aggiunta alla wishlist:` ,course.title);
+    this.wishlist.push(course);
+  }
 }
